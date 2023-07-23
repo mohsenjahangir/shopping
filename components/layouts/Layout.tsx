07c451ceed75e9281b3432de0aa152f0b0808ffc
mohsen jahangiri <moh7seven@yahoo.com>
2023-07-23 19:54:33 +0330

    fixed

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -8,7 +8,7 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <>
       <div className="">
-        <div className=""> 
+        <div className="  "> 
            <Header />
         </div>
         <div className="">
--- components/layouts/header/Header.tsx
+++ components/layouts/header/Header.tsx
@@ -1,3 +1,4 @@
+// Import icons and components
 import VitselIcon from "../../../public/svg/vistel.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
@@ -13,68 +14,66 @@ import DiscountIcon from "../../../public/svg/discount.svg";
 import Amazing from "../../../public/svg/amazing.svg";
 import { useState } from "react";
 
+// Define mock data for navigation items
+const navItems = [
+  {
+    name: "سوپر مارکت",
+    route: "/category/mac",
+    id: 0,
+    isBadge: false,
+    icon: <MarketIcon />,
+  },
+  {
+    name: "پرفروش ترین ها",
+    route: "/category/iphone",
+    id: 1,
+    isBadge: false,
+    icon: <MostSellIcon />,
+  },
+  {
+    name: "تخفیف ها و پیشنهادها",
+    route: "/category/appleWatch",
+    id: 2,
+    isBadge: false,
+    icon: <DiscountIcon />,
+  },
+  {
+    name: "شگفتانگیزها",
+    route: "/category/ipad",
+    id: 3,
+    isBadge: false,
+    icon: <Amazing />,
+  },
+  {
+    name: "سوالی دارید؟",
+    route: "/music",
+    id: 4,
+    isBadge: false,
+  },
+];
+
+const provinces = [
+  { value: "tehran", label: "تهران" },
+  { value: "mashhad", label: "مشهد" },
+  { value: "isfahan", label: "اصفهان" },
+];
+
 export const Header = () => {
-  const MockData = [
-    {
-      name: "سوپر مارکت",
-      route: "/category/mac",
-      id: 0,
-      isBadge: false,
-      icon: <MarketIcon />,
-    },
-    {
-      name: "پرفروش ترین ها",
-      route: "/category/iphone",
-      id: 1,
-      isBadge: false,
-      icon: <MostSellIcon />,
-    },
-    {
-      name: "تخفیف ها و پیشنهادها",
-      route: "/category/appleWatch",
-      id: 2,
-      isBadge: false,
-      icon: <DiscountIcon />,
-    },
-    {
-      name: "شگفت‌انگیزها",
-      route: "/category/ipad",
-      id: 3,
-      isBadge: false,
-      icon: <Amazing />,
-    },
-    {
-      name: "سوالی دارید؟",
-      route: "/music",
-      id: 4,
-      isBadge: false,
-    },
-  ];
-
-  const [provinces, setProvinces] = useState([
-    { value: "tehran", label: "تهران" },
-    { value: "mashhad", label: "مشهد" },
-    { value: "isfahan", label: "اصفهان" },
-    // Add more provinces here...
-  ]);
-
   return (
-    <div className="2xl:mx-10  lg:mx-3 mt-5 lg:-mt-5 lg:mb-44">
-      <div className=" items-center lg:invisible mb-1 flex justify-between mx-3">
+    <div className="lg:pb-5 border-b-2     bg-white  w-full">
+      <div className="items-center lg:hidden  flex justify-between py-2 mx-3">
         <div>
           <Category />
         </div>
-        <Link href="/" className=" ">
+        <Link href="/" className="">
           <VitselIcon />
         </Link>
-        <div className=" border-2 border-black px-2 rounded-md font-bold">
-          ?
-        </div>
+        <div className="border-2 border-black px-2 rounded-md font-bold">?</div>
       </div>
-      <div className=" flex flex-row justify-between mb-10 border-y-2 lg:border-y-0 p-2 lg:p-0 mx-3 lg:mx-0">
-        <div className=" basis-3/4 -mx-14 lg:-mx-0">
-          <div className=" flex  gap-2">
-            <Link href="/" className=" invisible lg:visible">
+      <div className="flex flex-row lg:grid lg:grid-cols-2 justify-between  w-full    border-y-2 lg:border-y-0  p-2   lg:pr-10 lg:pt-4  lg:fixed z-40 bg-white lg:pb-4   ">
+        <div className="basis-3/4 ">
+          <div className="flex gap-2">
+            <Link href="/" className=" bg-red-500 rounded-md ">
               <VitselIcon />
             </Link>
             <div className="relative col-span-11 text-gray-600 basis-3/4">
@@ -90,29 +89,30 @@ export const Header = () => {
             </div>
           </div>
         </div>
-        <div className=" flex  justify-end   gap-5 ">
-          <div className="  lg:gap-2  flex-row-reverse lg:flex-row flex  lg:border-2 p-2 rounded-lg">
+        <div className="flex justify-end gap-5 lg:ml-28 ">
+          <div className="lg:gap-2 flex-row-reverse lg:flex-row flex lg:border-2 p-2 rounded-lg">
             <div>
               <LoginIcon />
             </div>
-            <div className=" invisible lg:visible">ورود | ثبت نام</div>
+            <div className="invisible lg:visible">ورود | ثبت نام</div>
           </div>
           <div className="self-center">
             <CartIcon />
           </div>
         </div>
+        
       </div>
-      <div className=" lg:flex lg:justify-between  gap-4 invisible lg:visible">
-        <div className=" lg:flex lg:justify-center lg:gap-2">
+      <div className="lg:flex lg:justify-between gap-4  hidden lg:visible  p-2 lg:pr-10  lg:pt-24 ">
+        <div className="lg:flex lg:justify-center lg:gap-2">
           <div>
             <Dropdown />
           </div>
           <div className="lg:flex gap-4">
-            {MockData.map((item) => (
+            {navItems.map((item) => (
               <div className="hover:border-b-2 border-black" key={item.id}>
                 <Link
                   href={item.route}
-                  className=" truncate flex items-center gap-1"
+                  className="truncate flex items-center gap-1"
                 >
                   {item.icon}
                   {item.name}
@@ -121,22 +121,24 @@ export const Header = () => {
             ))}
           </div>
         </div>
-        <div className="font-thin  ml-4 flex">
+
+        <div className="font-thin ml-4 flex">
           <Location />
-          <p className=" truncate xl:w-44 lg:w-24 break-words ">
+          <p className="truncate xl:w-44 lg:w-24 break-words">
             لطفا شهر خود را انتخاب کنید
           </p>
         </div>
       </div>
-      <div className="lg:invisible  -mt-52 mx-3 z-10  ">
-        <div className="  flex items-center ">
-          <div className="">
+
+      <div className="lg:hidden visible mb-4  mx-3 z-10">
+        <div className="flex items-center">
+          <div>
             <Location />
           </div>
           <select
             id="provinces"
             name="provinces"
-            className=" bg-transparent  border-gray-500 text-gray-500 py-2 px-3 leading-tight focus:outline-none focus:border-indigo-500"
+            className="bg-transparent border-gray-500 text-gray-500 py-2 px-3 leading-tight focus:outline-none focus:border-indigo-500"
           >
             <option value="" disabled selected hidden>
               شهر خورد را انتخاب کنید
